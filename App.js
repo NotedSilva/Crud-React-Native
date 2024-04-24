72757e81a538794683de12a8d28ff94f996c105e
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-04-24 16:57:28 -0300

    criação dos props e estados das funções de deletar e editar

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, FlatList, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View, } from 'react-native';
+import { ActivityIndicator, FlatList, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 import ShoppingItem from './components/ShoppingItem';
-import {MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import { useEffect, useState } from 'react';
-import {app ,db ,getFirestore, collection, addDoc, getDocs} from "./firebase/index.js";
+import { app, db, getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "./firebase/index.js";
 
 export default function App() {
-  const [title,setTitle] = useState("");
-
-  const [shoppingList,setShoppingList] = useState([]);
+  const [title, setTitle] = useState("");
+  const [newTitle, setNewTitle] = useState(""); // Novo estado para controlar o novo nome do item
+  const [shoppingList, setShoppingList] = useState([]);
 
   const addShoppingItem = async () => {
     try {
@@ -27,14 +27,38 @@ export default function App() {
   const getShoppingList = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, "shopping"));
-      const tempList = []; // Crie uma lista temporária para armazenar os itens
+      const tempList = [];
       querySnapshot.forEach((doc) => {
         console.log(doc.id, doc.data());
         tempList.push({ ...doc.data(), id: doc.id });
       });
-      setShoppingList(tempList); 
+      setShoppingList(tempList);
     } catch (error) {
       console.error("Error getting documents: ", error);
+    }
+  };
+
+  const deleteShoppingList = async () => {
+    const querySnapshot = await getDocs(collection(db, "shopping"));
+
+    querySnapshot.docs.map(async (doc) => {
+      await deleteDoc(doc.ref);
+    });
+
+    getShoppingList();
+  }
+
+  // Função para atualizar o nome do item
+  const updateShoppingItem = async (id) => {
+    try {
+      const shoppingRef = doc(db, "shopping", id);
+      await updateDoc(shoppingRef, {
+        title: newTitle,
+      });
+      setNewTitle("");
+      getShoppingList();
+    } catch (error) {
+      console.error("Error updating document: ", error);
     }
   };
 
@@ -46,34 +70,42 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         {/* heading */}
-          <Text style={styles.heading}>Lista de Compras</Text>
+        <Text style={styles.heading}>Lista de Compras</Text>
         {/* number of shopping items */}
-          <Text style={styles.numberOfItems}>0</Text>
+        <Text style={styles.numberOfItems}>{shoppingList.length}</Text>
         {/* delete all */}
-          <Pressable>
-            <MaterialIcons name="delete" size={30} color="black" />
-          </Pressable>
+        <Pressable onPress={deleteShoppingList}>
+          <MaterialIcons name="delete" size={30} color="black" />
+        </Pressable>
       </View>
 
       {
 
         shoppingList.length > 0 ?
-      <FlatList
-        data={shoppingList}
-        renderItem={({item}) =><ShoppingItem title={item.title}/>}
-        keyExtractor={item=>item.id}
-      />
-       : (
-      <ActivityIndicator/>
-    )}
+          <FlatList
+            data={shoppingList}
+            renderItem={({ item }) => (
+              <ShoppingItem
+                title={item.title}
+                isChecked={item.isChecked}
+                id={item.id}
+                getShoppingList={getShoppingList}
+                updateShoppingItem={updateShoppingItem} // Passando a função de atualização como prop
+              />
+            )}
+            keyExtractor={item => item.id}
+          />
+          : (
+            <ActivityIndicator />
+          )}
 
       {/* text input */}
       <TextInput
-      placeholder="Insira o nome do item"
-      style={styles.input}
-      value={title}
-      onChangeText={(text) => setTitle(text)}
-      onSubmitEditing={addShoppingItem}
+        placeholder="Insira o nome do item"
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        onSubmitEditing={addShoppingItem}
       />
     </SafeAreaView>
   );
